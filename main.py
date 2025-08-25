68eaca188b974749cf4c0f82d0132a9a192ee942
Adrian B <adrianny90@wp.pl>
2025-08-25 17:23:27 +0200

    add mangum

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 This module initializes the FastAPI app and defines API endpoints for the project.
 """
 from fastapi import FastAPI
-
+from mangum import Mangum
 app = FastAPI()
 
 @app.get('/')
 def get_all():
     """Take a number n and return the square of n."""
-    return "hello from fast api"
+    return {"message": "hello from Erkner"}
+
+handler = Mangum(app=app)
